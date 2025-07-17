5b196c73bf2b989f3b8afaa51b4ec7527184635a
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2025-07-17 12:23:46 +0100

    🚧 chore: Setup Assets Package

--- packages/assets/lib/assets.dart
+++ packages/assets/lib/assets.dart
@@ -1,5 +1,5 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// Assets Definitions for Civic24
+library app_assets;
+
+export 'asset_gen/assets.gen.dart';
+export 'asset_gen/fonts.gen.dart';
