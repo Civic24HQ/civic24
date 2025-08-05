7d8d86e70dcc8590e2dbaee2c668cb9b8fa14ba5
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2025-08-05 12:34:08 +0100

    🚧 chore: Update CI/CD and pubspec file for both Citizen and Admin App

--- apps/admin/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/admin/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,9 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
 import firebase_analytics
+import firebase_app_check
 import firebase_auth
 import firebase_core
 import firebase_crashlytics
@@ -13,7 +15,9 @@ import firebase_messaging
 import firebase_storage
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
