ea82686efe1b3b414cb9323ab879fd922ebfd551
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2025-08-08 14:03:12 +0100

    🚧 chore: Add Text Input Formatter for Text Fields

---
+++ packages/utils/lib/src/text_input_formatters.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/services.dart';
+
+/// A formatter that doesn't allow spaces
+final TextInputFormatter noSpacesFormatter = FilteringTextInputFormatter.deny(' ');
+
+/// A formatter that allows only Alphabets
+final TextInputFormatter alphabetsOnlyFormatter = FilteringTextInputFormatter.allow(RegExp('[a-zA-Z]'));
+
+/// A formatter that allows Alphabet and spacing
+final TextInputFormatter alphabetsWithSpaceFormatter = FilteringTextInputFormatter.allow(RegExp('[a-zA-Z ]'));
+
+/// A formatter that allows only numbers
+final TextInputFormatter numbersOnlyFormatter = FilteringTextInputFormatter.digitsOnly;
