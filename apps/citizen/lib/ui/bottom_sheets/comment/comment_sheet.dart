e3c6050981f4307128fc1d749c9b76399028210f
Caleb Jesusegun <jesuseguncaleb@gmail.com>
2025-08-22 18:32:12 +0100

    🚧 chore: Update Citizen App

--- apps/citizen/lib/ui/bottom_sheets/comment/comment_sheet.dart
+++ apps/citizen/lib/ui/bottom_sheets/comment/comment_sheet.dart
@@ -1,56 +1,97 @@
+import 'package:citizen/ui/bottom_sheets/comment/comment_sheet_model.dart';
+import 'package:components/components.dart';
 import 'package:flutter/material.dart';
-import 'package:citizen/ui/common/app_colors.dart';
-import 'package:citizen/ui/common/ui_helpers.dart';
+import 'package:localization/localization.dart';
+import 'package:models/models.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
-import 'comment_sheet_model.dart';
+import 'package:styles/styles.dart';
+import 'package:utils/utils.dart';
 
 class CommentSheet extends StackedView<CommentSheetModel> {
-  final Function(SheetResponse response)? completer;
-  final SheetRequest request;
-  const CommentSheet({Key? key, required this.completer, required this.request})
-    : super(key: key);
+  const CommentSheet({required this.completer, required this.request, super.key});
+
+  final SheetRequest<dynamic> request;
+  final Function(SheetResponse<dynamic>) completer;
 
   @override
-  Widget builder(
-    BuildContext context,
-    CommentSheetModel viewModel,
-    Widget? child,
-  ) {
+  Widget builder(BuildContext context, CommentSheetModel viewModel, Widget? child) {
+    final deviceHeight = MediaQuery.of(context).size.height;
+
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-      decoration: const BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            request.title ?? 'Hello Stacked Sheet!!',
-            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
-          ),
-          if (request.description != null) ...[
-            verticalSpaceTiny,
-            Text(
-              request.description!,
-              style: const TextStyle(fontSize: 14, color: kcMediumGrey),
-              maxLines: 3,
-              softWrap: true,
+      constraints: BoxConstraints(maxHeight: deviceHeight * 0.7),
+      padding: AppEdgeInsets.padding16,
+      decoration: BoxDecoration(color: context.surfaceVariant, borderRadius: AppBorderRadius.topRadius16),
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppSpacing.medium,
+            Center(
+              child: Container(
+                width: 64,
+                height: 5,
+                decoration: BoxDecoration(color: context.neutralLow, borderRadius: AppBorderRadius.radius12),
+              ),
+            ),
+            AppSpacing.normal,
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 10,
+                padding: AppEdgeInsets.padding12,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    horizontalTitleGap: AppDimensions.size12,
+                    leading: ClipOval(
+                      child: Container(
+                        width: AppDimensions.size40,
+                        height: AppDimensions.size40,
+                        color: context.neutralLow,
+                        child: AppCachedImage(imageUrl: viewModel.comments[index].userImageUrl),
+                      ),
+                    ),
+                    title: Text(
+                      viewModel.comments[index].fullName,
+                      style: context.headlineSmall?.copyWith(fontSize: AppDimensions.size16),
+                    ),
+                    subtitle: Text(
+                      viewModel.comments[index].comment,
+                      style: context.bodyLarge?.copyWith(color: context.neutralHigh, fontSize: AppDimensions.size14),
+                    ),
+                    trailing: Text(
+                      viewModel.comments[index].createdAt.timeAgoInWords,
+                      style: context.bodyLarge?.copyWith(color: context.neutralHigh, fontSize: AppDimensions.size14),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      softWrap: true,
+                      textAlign: TextAlign.start,
+                    ),
+                  );
+                },
+              ),
+            ),
+            AppSpacing.normal,
+            AppTextField(
+              hintText: l10n.generalAddComment,
+              filled: true,
+              controller: TextEditingController(),
+              fillColor: context.surface,
+              enabledBorder: context.surface,
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.send, size: AppDimensions.size24, color: context.primary),
+              ),
             ),
           ],
-          verticalSpaceLarge,
-        ],
+        ),
       ),
     );
   }
 
   @override
   CommentSheetModel viewModelBuilder(BuildContext context) =>
-      CommentSheetModel();
+      CommentSheetModel(request.data as List<CommentData>? ?? []);
 }
--- apps/citizen/lib/ui/views/home/home_view.dart
+++ apps/citizen/lib/ui/views/home/home_view.dart
@@ -1,6 +1,10 @@
+import 'package:citizen/ui/shared/src/assets.gen.dart';
 import 'package:citizen/ui/views/home/home_viewmodel.dart';
+import 'package:components/components.dart';
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
 import 'package:stacked/stacked.dart';
+import 'package:styles/styles.dart';
 
 class HomeView extends StackedView<HomeViewModel> {
   const HomeView({super.key});
@@ -8,9 +12,40 @@ class HomeView extends StackedView<HomeViewModel> {
   @override
   Widget builder(BuildContext context, HomeViewModel viewModel, Widget? child) {
     return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.only(left: 25, right: 25),
-        child: const Center(child: Text('Home View')),
+      backgroundColor: context.surface,
+      appBar: AppBar(
+        toolbarHeight: AppDimensions.size64,
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: context.surface,
+        scrolledUnderElevation: 0,
+        automaticallyImplyLeading: false,
+        shape: const Border(bottom: BorderSide(color: Colors.transparent)),
+        title: Assets.png.civic24SplashScreenIOS.image(width: AppDimensions.size72, height: AppDimensions.size48),
+      ),
+      body: AppTabs(
+        tabs: [
+          AppTab(
+            label: l10n.generalAllIssues,
+            view: CustomScrollView(
+              slivers: [
+                SliverList(
+                  delegate: SliverChildBuilderDelegate((context, index) {
+                    return AppPost(onTapComment: () => viewModel.viewComment(), postData: viewModel.postList[index]);
+                  }, childCount: viewModel.postList.length),
+                ),
+              ],
+            ),
+          ),
+          AppTab(
+            label: l10n.generalTrending,
+            view: Center(child: Text(l10n.generalTrending)),
+          ),
+          AppTab(
+            label: l10n.generalCategory,
+            view: Center(child: Text(l10n.generalCategory)),
+          ),
+        ],
       ),
     );
   }
